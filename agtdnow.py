be29afb578f06445066477483ca07dcce3f9b455
16242 <yang6824@gmail.com>
2021-10-27 09:04:38 +0800

    0.3 减少未来函数

---
+++ agtdnow.py
@@ -0,0 +1,44 @@
+import requests
+import os
+import re
+import json
+import time
+import mysql.connector
+import math
+import numpy as np
+import talib
+from decimal import Decimal
+from decimal import getcontext
+
+
+def get(tm,count='25'):#type1,2,3,4,5:1m,5m,15m,30m,60m qid 6,agtd,13 xag
+ url = "https://official.gkoudai.com/officialNetworkApi/CandleStickV2?qid=13&type=5&count="+str(count)+"&ts="+str(tm)
+ header = {'epid': 'a6c89023-9472-4f30-81cf-8c7dea62aae5'}
+ r=requests.post(url,headers=header)
+ candle=json.loads(r.text)['data']['candle']
+
+
+ return candle
+
+
+
+
+ma=13
+
+
+count=25
+while True:
+ tm=int(time.time()*1000)
+ candle=get(tm,count)
+ li=[]
+ rate=[]
+ for i in range(0,len(candle)):
+  li.append(float(candle[i]['c']))
+ li.reverse()
+ #print(li)
+ for i in range(int(count)-ma):
+  rate.append(round((sum(li[i:i+ma])-sum(li[i+1:i+1+ma]))/sum(li[i+1:i+1+ma])*100000,3))
+ print(rate)
+ time.sleep(60)
+
+input()
